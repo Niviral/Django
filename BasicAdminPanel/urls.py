179618e72ec5eaa119a2c1d95652c5411d62db9c
Marcin Bienias <Marcin.Bienias@hotmail.com>
2020-11-11 11:46:18 +0100

    Fully working login and password reset forms

--- BasicAdminPanel/urls.py
+++ BasicAdminPanel/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
-    re_path('^admin/$', admin.site.urls),
+    re_path('^admin/', admin.site.urls),
     re_path(r'^', include('home.urls')),
     re_path('^account/',include('django.contrib.auth.urls')),
 ]
