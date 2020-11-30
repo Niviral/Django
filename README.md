13f19ad8ec11207adc48efac3e0f8fe9d0bb2f5d
Marcin Bienias <Marcin.Bienias@hotmail.com>
2020-11-30 19:37:04 +0100

    Registration form + Readme.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Simple basic Django project.
-
+Simple basic Django (3.1.2) project created for learning purpose.
 
 Project created with use of [SB Admin 2](https://startbootstrap.com/themes/sb-admin-2/) open source admin dashboard theme for [Bootstrap](https://getbootstrap.com/) created by [Start Bootstrap](https://startbootstrap.com/).
+
+All application needed for project to work are docker containers for easy deployment.</br>
+All `docker-compose.yml` and `.env` example file provided.
+
+Before deployment please export Django Secret key to your enviroment variable:</br>
+```export DJANGO_SECRET=="$(python manage.py shell -c 'from django.core.management import utils; print(utils.get_random_secret_key())')"```
--- home/views.py
+++ home/views.py
@@ -1,8 +1,25 @@
 from django.http import request
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home/welcome.html')
+
+def register(request):
+
+    if request.user.is_authenticated:
+        return redirect('', username=request.user.username)
+
+    if request.method == 'POST':
+        f = UserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            # messages.success(request, 'Account created successfully')
+            return redirect('signup')
+
+    else:
+        f = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': f})
